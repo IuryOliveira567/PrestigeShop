fccc58e22eb4ee7aa0481cde22a34c8d776f8a12
IuryOliveira567 <Iury.Oliveira567@gmail.com>
2023-11-03 09:59:35 -0300

    buid: bootstrap vue imported

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 createApp(App).mount('#app')
